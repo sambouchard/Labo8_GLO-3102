c904cf3ca92249007afedb86e1231bf868b92793
ULAVAL\SABOU350 <samuel.bouchard.7@ulaval.ca>
2018-03-25 13:40:25 -0400

    Work in progress

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,5 @@
-import Cookies from 'js-cookie'
-const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const corsOptions = {
     origin: '*',
@@ -8,23 +7,33 @@ const corsOptions = {
     credentials: true
 };
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
+
+
+const users = [{username: 'sam', password: '1234'}, {username:'vaun' , password: 'orage'}];
 
 const app = express();
-const users = [{username: 'sam', password: '1234'}, {username:'vaun' , password: 'orage'}];
+const bodyParser = require('body-parser');
+
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 app.post('/login',function (req,res) {
-    console.log('234');
-
+    let usertoidentify = req.body.username;
+    let passtoidentify = req.body.password;
+    for(let i = 0; i < users.length; i++){
+        if(usertoidentify == users[i].username && passtoidentify == users[i].password){
+            console.log('ok')
+        }
+    }
 });
 
 app.listen(port, function() {
     console.log('Server listening.')
 });
+
 
 
 
